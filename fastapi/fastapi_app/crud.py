34fc9c372b4364f90fc1194e6dd06de0f2f1e9f5
sawa <xj558y2d0290acc@gmail.com>
2025-09-11 00:12:30 +0900

    一旦バックエンド仮作成。フロントが準備不足なのでそちらを先に実装

--- fastapi/fastapi_app/crud.py
+++ fastapi/fastapi_app/crud.py
@@ -1,12 +1,25 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
 
-def get_posts(db: Session):
-    return db.query(models.Post).order_by(models.Post.id.desc()).all()
-
-def create_post(db: Session, post: schemas.PostCreate):
-    db_post = models.Post(**post.dict())
-    db.add(db_post)
+def create_schedule(db: Session, schedule: schemas.ScheduleCreate):
+    db_schedule = models.Schedule(title=schedule.title)
+    db.add(db_schedule)
     db.commit()
-    db.refresh(db_post)
-    return db_post
+    db.refresh(db_schedule)
+
+    # bulk insert timeslots
+    timeslots = [
+        models.ScheduleTimeslot(
+            schedule_uuid=db_schedule.uuid,
+            start_time=t.start_time,
+            end_time=t.end_time
+        )
+        for t in schedule.timeslots
+    ]
+    db.add_all(timeslots)
+    db.commit()
+    db.refresh(db_schedule)
+    return db_schedule
+
+def get_schedule(db: Session, schedule_uuid):
+    return db.query(models.Schedule).filter(models.Schedule.uuid == schedule_uuid).first()
--- fastapi/fastapi_app/schemas.py
+++ fastapi/fastapi_app/schemas.py
@@ -1,15 +1,28 @@
 from pydantic import BaseModel
+from datetime import datetime
+from typing import List
+from uuid import UUID
 
-class PostBase(BaseModel):
-    title: str
-    username: str
-    text: str
+class TimeslotBase(BaseModel):
+    start_time: datetime
+    end_time: datetime
 
-class PostCreate(PostBase):
+class TimeslotCreate(TimeslotBase):
     pass
 
-class PostRead(PostBase):
+class TimeslotRead(TimeslotBase):
     id: int
+    class Config:
+        orm_mode = True
 
+class ScheduleBase(BaseModel):
+    title: str
+
+class ScheduleCreate(ScheduleBase):
+    timeslots: List[TimeslotCreate]
+
+class ScheduleRead(ScheduleBase):
+    uuid: UUID
+    timeslots: List[TimeslotRead]
     class Config:
         orm_mode = True
